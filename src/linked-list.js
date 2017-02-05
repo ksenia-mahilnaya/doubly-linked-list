92b29f4d80ef5408e883b4a6b49fed31ff2d015e
ksenia-mahilnaya <ksenia.vlady@gmail.com>
2017-02-05 20:29:38 +0200

    doubly-linked-list/fix: fixed length, deleted variables

--- src/linked-list.js
+++ src/linked-list.js
@@ -6,12 +6,12 @@ class LinkedList {
         this._tail = null;
         this.length = 0;
     }
-    
+
     append(data) {
         var node = new Node();
         node.data = data;
-       
-        
+
+
         if (this.length) {
             this._tail.next = node;
             node.prev = this._tail;
@@ -20,157 +20,151 @@ class LinkedList {
             this._head = node;
             this._tail = node;
         }
-        
+
         this.length++;
-        
+
         return this;
     }
-    
+
     head() {
-      return this._head.data;  
+      return this._head.data;
     }
-    
+
     tail() {
         return this._tail.data;
     }
-    
+
     at(index) {
         var currentNode = this._head,
             length = this.length,
             counter = 0,
             message = {failure : 'Non-existent node'};
-           
-        
-        if (length === 0 || index < 0 || index > length) {
+
+
+        if (length === 0 || index < 0 || index > length - 1) {
             throw new Error(message.failure);
         }
-        
+
         while (counter < index) {
             currentNode = currentNode.next;
             counter++;
         }
         return currentNode.data;
     }
-    
+
     deleteAt(index) {
         var currentNode = this._head,
             length = this.length,
             counter = 0,
-            message = {
-                failure : 'Non-existent node in this list'
-                },
+            message = {failure : 'Non-existent node in this list'},
             beforeNodeToDelete = null,
             nodeToDelete = null,
             deletedNode = null,
             afterNodeToDelete = null;
-        
-        if (length === 0 || index < 0 || index > length) {
+
+        if (length === 0 || index < 0 || index > length - 1) {
             throw new Error(message.failure);
         }
-        
+
         if (index === 0) {
             this._head = currentNode.next;
-            
+
             if (this._head) {
                 this._head.prev = null;
             } else {
                 this._tail = null;
             }
-            
-        } else if (index === this.length) {
+
+        } else if (index === this.length - 1) {
             this._tail = this._tail.prev;
-            this._tail.next = null; 
-            
-        } else {   
+            this._tail.next = null;
+
+        } else {
             while (counter < index) {
                 currentNode = currentNode.next;
                 counter++;
             }
-            
+
             beforeNodeToDelete = currentNode.prev;
             nodeToDelete = currentNode;
             afterNodeToDelete = currentNode.next;
-            
+
             beforeNodeToDelete.next = afterNodeToDelete;
             afterNodeToDelete.prev = beforeNodeToDelete;
-            deletedNode = nodeToDelete;
             nodeToDelete = null;
         }
-        
+
       this.length--;
-    
+
       return this;
-        
-        
+
+
     }
-    
-       insertAt(index, data) {
+
+    insertAt(index, data) {
         var node = new Node(),
             currentNode = this._head,
             length = this.length,
             counter = 0,
             beforeNodeToInsert = null,
             afterNodeToInsert = null,
-            message = {
-                failure : 'Wrong index!'
-                }
-           
+            message = {failure : 'Wrong index!'};
+
            node.data = data;
-           
-       
-        
+
         if (index < 0 || index > length) {
             throw new Error(message.failure);
         }
-        
+
         if (index === 0) {
             if (this.length) {
                 this._head = node;
                 this._head.next = currentNode;
                 currentNode.prev = this._head;
-               
+
             } else {
                 this._head = node;
                 this._tail = node;
             }
-            
+
         } else if (index === length) {
             currentNode = this._tail;
             this._tail.next = node;
             node.prev = currentNode;
             this._tail = node;
-            
+
         } else {
             while (counter < index) {
                 currentNode = currentNode.next;
                 counter++;
             }
-            
+
             beforeNodeToInsert = currentNode.prev;
             afterNodeToInsert = currentNode;
-            
+
             beforeNodeToInsert.next = node;
             afterNodeToInsert.prev = node;
             node.next = afterNodeToInsert;
             node.prev = beforeNodeToInsert;
         }
-        
-           
+
+
         this.length++;
-           
+
         return this;
     }
-    
+
     isEmpty() {
+
         if (this.length === 0) {
             return true;
         } else {
             return false;
         }
     }
-    
+
      clear() {
-        
+
         if (this.length) {
             this._head.data = null;
             this._tail.data = null;
@@ -179,51 +173,50 @@ class LinkedList {
             this._head = null;
             this._tail = null;
         }
-        
+
     return this;
-        
+
     }
-    
-     reverse() {
+
+    reverse() {
         var currentNode = this._head,
-            temp = null;
-        
+            tmp = null;
+
         this._tail = this._head;
-        
-        while (currentNode != null) {
-            temp = currentNode.next;
+
+        while (currentNode !== null) {
+            tmp = currentNode.next;
             currentNode.next = currentNode.prev;
-            currentNode.prev = temp;
-            
-            if (currentNode.prev == null) {
+            currentNode.prev = tmp;
+
+            if (currentNode.prev === null) {
                 this._head = currentNode;
             }
-            
             currentNode = currentNode.prev;
         }
+
     return this;
-        
+
     }
 
     indexOf(data) {
         var node = new Node(),
             currentNode = this._head,
             counter = 0;
-           
+
         node.data = data;
-        
+
         while (currentNode.data !== node.data) {
             currentNode = currentNode.next;
             if (currentNode === null) {
               counter = -1;
-              break;  
+              break;
             }
             counter++;
         }
-        
+
         return counter;
-             
-        
+
     }
 
 }
